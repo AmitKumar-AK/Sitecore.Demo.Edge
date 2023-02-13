e535e151dac370ee83278a87d17add51ce45716f
Amit Kumar <amitkumar_mca04@hotmail.com>
2023-02-13 11:53:29 +0000

    Serilog configuration added.

--- api/src/Sitecore.Demo.Edge.API/Program.cs
+++ api/src/Sitecore.Demo.Edge.API/Program.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Serilog;
 
 namespace Sitecore.Demo.Edge.API
 {
@@ -13,11 +6,12 @@ namespace Sitecore.Demo.Edge.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+               .UseSerilog()  
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- api/src/Sitecore.Demo.Edge.API/Startup.cs
+++ api/src/Sitecore.Demo.Edge.API/Startup.cs
@@ -1,16 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
+using Serilog;
 using Sitecore.Demo.Edge.Application.DependencyInjection;
 using Sitecore.Demo.Edge.Infrastructure.OpenAQAir;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Sitecore.Demo.Edge.API
 {
@@ -19,7 +10,10 @@ namespace Sitecore.Demo.Edge.API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+          Configuration = configuration;
+          Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration).CreateLogger();
+    }
 
         public IConfiguration Configuration { get; }
 
